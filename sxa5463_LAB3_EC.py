7e5a4f88455863fff1e2f68e9284eb5a1ebefb1b
razaali01 <57025721+razaali01@users.noreply.github.com>
2022-04-11 12:55:58 -0500

    Add files via upload

---
+++ sxa5463_LAB3_EC.py
@@ -0,0 +1,68 @@
+# Name: Syed Ali
+# NetID: 1001725463
+# Date Turned In: 4/7/2022
+# Python Version 3.10
+
+# For the extra credit I added the ^ operator
+# to represent an exponent
+
+import os
+
+
+# function to calculate values in RPN
+def RPNCalculator(RPN, operators):
+    # making a stack
+    stack = []
+
+    # separating the string
+    SplitRPN = RPN.split()
+
+    for x in SplitRPN:
+        # check if x is part of the operators list
+        if x in operators:
+
+            val2 = stack.pop()
+
+            val1 = stack.pop()
+
+            # check is operator is addition
+            if x == '+':
+                results = val1 + val2
+            # check is operator is subtraction
+            elif x == '-':
+                results = val1 - val2
+            # check is operator is multiplication
+            elif x == '*':
+                results = val1 * val2
+            # check is operator is division
+            elif x == '/':
+                results = val1 / val2
+            elif x == '^':
+                results = val1 ** val2
+
+            # store results in the stack
+            stack.append(results)
+        else:
+            # if not an operator then it is a number so push it
+            # back to the stack
+            stack.append(float(x))
+
+    # return the result
+    return stack.pop()
+
+
+# opening file to read
+Fileinput = open('input_RPN_EC.txt', 'r')
+
+# saving lines in the file
+StuffInFile = Fileinput.readlines()
+
+# stating which operators are allowed
+operators = ["+", "-", "*", "/", "^"]
+
+for x in StuffInFile:
+    RPN = x
+    print(RPNCalculator(RPN, operators))
+
+# closing file
+Fileinput.close()
